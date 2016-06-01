d2e85621589cf15b0da0742d0c9bc6ffc3d4f819
Ian Macalinao <me@ian.pw>
2016-05-31 21:26:35 -0400

    Add key return method

--- keypool.go
+++ keypool.go
@@ -5,38 +5,44 @@ import "time"
 // Key represents an API key
 type Key struct {
 	Value    string
-	LastUsed time.Time
+	lastUsed time.Time
+	pool     *Keypool
+}
+
+// Return puts a key back into the pool
+func (k *Key) Return() {
+	k.lastUsed = time.Now()
+	k.pool.keys <- k
 }
 
 // Keypool stores keys
 type Keypool struct {
-	Keys chan *Key
-	Rate time.Duration
+	keys chan *Key
+	rate time.Duration
 }
 
 // New creates a new key
 func New(keys []string, rate time.Duration) *Keypool {
+	pool := &Keypool{
+		rate: rate,
+	}
 	kc := make(chan *Key, len(keys))
 	for _, key := range keys {
 		kc <- &Key{
 			Value: key,
+			pool:  pool,
 		}
 	}
-	return &Keypool{
-		Keys: kc,
-		Rate: rate,
-	}
+	pool.keys = kc
+	return pool
 }
 
 // Fetch gets a new key
-func (k *Keypool) Fetch() string {
-	next := <-k.Keys
-	dur := time.Since(next.LastUsed)
-	if dur < k.Rate {
-		time.Sleep(k.Rate - dur)
+func (k *Keypool) Fetch() *Key {
+	next := <-k.keys
+	dur := time.Since(next.lastUsed)
+	if dur < k.rate {
+		time.Sleep(k.rate - dur)
 	}
-	v := next.Value
-	next.LastUsed = time.Now()
-	k.Keys <- next
-	return v
+	return next
 }
--- keypool_test.go
+++ keypool_test.go
@@ -10,22 +10,26 @@ import (
 func TestKeypoolNoRateLimit(t *testing.T) {
 	keys := []string{"a", "b", "c", "d"}
 	k := New(keys, 0*time.Millisecond)
-	assert.Equal(t, k.Fetch(), "a")
-	assert.Equal(t, k.Fetch(), "b")
-	assert.Equal(t, k.Fetch(), "c")
-	assert.Equal(t, k.Fetch(), "d")
-	assert.Equal(t, k.Fetch(), "a")
+	key := k.Fetch()
+	assert.Equal(t, key.Value, "a")
+	assert.Equal(t, k.Fetch().Value, "b")
+	assert.Equal(t, k.Fetch().Value, "c")
+	assert.Equal(t, k.Fetch().Value, "d")
+	key.Return()
+	assert.Equal(t, k.Fetch().Value, "a")
 }
 
 func TestKeypoolWithRateLimit(t *testing.T) {
 	keys := []string{"a", "b", "c", "d"}
 	k := New(keys, 10*time.Millisecond)
 	start := time.Now()
-	assert.Equal(t, k.Fetch(), "a")
-	assert.Equal(t, k.Fetch(), "b")
-	assert.Equal(t, k.Fetch(), "c")
-	assert.Equal(t, k.Fetch(), "d")
+	key := k.Fetch()
+	assert.Equal(t, key.Value, "a")
+	assert.Equal(t, k.Fetch().Value, "b")
+	assert.Equal(t, k.Fetch().Value, "c")
+	assert.Equal(t, k.Fetch().Value, "d")
 	assert.True(t, time.Since(start) < 1*time.Millisecond, "No sleep if no dupes")
-	assert.Equal(t, k.Fetch(), "a")
+	key.Return()
+	assert.Equal(t, k.Fetch().Value, "a")
 	assert.False(t, time.Since(start) < 1*time.Millisecond, "Sleep if duration")
 }
